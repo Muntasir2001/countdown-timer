0df1e2d311c4267052252ffdcf3d013b9183a98c
Rehan Nagoor <rehannags@yahoo.com>
2021-09-13 17:20:33 -0400

    updated card related stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Socials from './components/SocialMedia'
 
 function App() {
 	return (
 		<>
 			<h2>We are launching soon</h2>
+
+			<Socials />
 		</>
 	);
 }
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = ({}) => {
+    return (
+        <>
+            <div>
+                <div classname="upper-layer">
+                    
+                </div>
+                <div classname="lower layer">
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
